77bb45c08b4933df914bfdfa0969809078f3aa57
VincentFerrigan <vincent.ferrigan@gmail.com>
2022-10-28 14:06:22 +0200

    Adding comments to and TODO:s for MyGraph

--- src/MyGraph.jl
+++ src/MyGraph.jl
@@ -3,24 +3,32 @@
     MyGraph
 
 Author: Vincent Ferrigan, ferrigan@kth.se
-Date: 2022-10-18
+Date: 2022-10-28
 Notes:
 
 # TODO:
-* ...
+* lookup or similar 
+* BFS
+* DFS
+* iterate
+* pretty printing (show)
+* Dijkstra's algorithm
 
 Contains:
 # Types
-* 
-* 
+* Edge
+* Graph # will have to rename it to UndirWeightedGraph :> Graph
 
 # Utils
 ## Base overload utils/methods
-* 
+* isequal
+* isless
+* add!
 
 ## Functions/Methods
-* 
-* 
+* either
+* other
+* weight
 
 """
 module MyGraph
